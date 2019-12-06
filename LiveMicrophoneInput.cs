94d89a2bc23fd62afdb2069f7f9c4e8d46ffda0f
enjsof <51922991+enjsof@users.noreply.github.com>
2019-12-06 09:10:12 -0800

    Add files via upload

---
+++ LiveMicrophoneInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent (typeof(AudioSource))]
+public class LiveMicrophoneInput : MonoBehaviour {
+
+
+    AudioSource audio;
+    // Use this for initialization
+    void Start () {
+
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+        
+        audio = GetComponent<AudioSource>();
+
+       
+        audio.clip = Microphone.Start(Microphone.devices[0], true, 1, 22050);
+        
+       
+
+
+        audio.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audio.Play();
+        
+        
+    }
+
+}

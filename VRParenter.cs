5711ae4b3371ea18e7b5b68d9edfef349f6a690f
Andy Fresnanda <51922991+fre5@users.noreply.github.com>
2019-12-31 16:40:15 -0800

    init

---
+++ VRParenter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRParenter : MonoBehaviour
+{
+    public GameObject headref;
+    public GameObject leftHandref;
+    public GameObject rightHandref;
+    public Vector3 head;
+    public Vector3 leftHand;
+    public Vector3 rightHand;
+
+
+
+    private CharacterController charControl;
+
+    private CharacterControllerVR charControlVR;
+
+    private void Start()
+    {
+        charControl = GetComponent<CharacterController>();
+        charControlVR = GetComponent<CharacterControllerVR>();
+    }
+
+
+    private void Update()
+    {
+        headref.transform.position = head;
+        leftHandref.transform.position = leftHand;
+        rightHandref.transform.position = rightHand;
+
+        
+
+
+        if(Input.GetKeyUp(KeyCode.I))
+        {
+            charControl.enabled = !charControl.enabled;
+            charControlVR.enabled = !charControlVR.enabled;
+        }
+    }
+}
+
+

8cdde8e34eb24fd4b70c58e8de84208823106be2
Andy Fresnanda <51922991+fre5@users.noreply.github.com>
2021-11-13 09:14:56 -0800

    Prettify

--- LiveMicrophoneInput.cs
+++ LiveMicrophoneInput.cs
@@ -6,29 +6,19 @@ using UnityEngine.Audio;
 [RequireComponent (typeof(AudioSource))]
 public class LiveMicrophoneInput : MonoBehaviour {
 
-
     AudioSource audio;
     // Use this for initialization
     void Start () {
-
+    
         foreach (var device in Microphone.devices)
         {
             Debug.Log("Name: " + device);
         }
         
-        audio = GetComponent<AudioSource>();
-
-       
+        audio = GetComponent<AudioSource>();   
         audio.clip = Microphone.Start(Microphone.devices[0], true, 1, 22050);
-        
-       
-
-
         audio.loop = true;
         while (!(Microphone.GetPosition(null) > 0)) { }
         audio.Play();
-        
-        
     }
-
 }
